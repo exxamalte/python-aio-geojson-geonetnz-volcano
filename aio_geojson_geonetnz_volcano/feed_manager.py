1c6fd5d3b9631a425d2c4d52e02f8c3d5b4cf12b
Malte Franken <exxamalte@users.noreply.github.com>
2019-09-19 17:42:31 +1000

    feed manager keeps all entries in memory

--- aio_geojson_geonetnz_volcano/feed_manager.py
+++ aio_geojson_geonetnz_volcano/feed_manager.py
@@ -1,8 +1,12 @@
 """Feed Manager for GeoNet NZ Volcanic Alert Level feed."""
+import logging
+
 from aio_geojson_client.feed_manager import FeedManagerBase
 from aiohttp import ClientSession
 
 from .feed import GeonetnzVolcanoFeed
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class GeonetnzVolcanoFeedManager(FeedManagerBase):
@@ -18,3 +22,16 @@ class GeonetnzVolcanoFeedManager(FeedManagerBase):
             filter_radius=filter_radius)
         super().__init__(feed, generate_callback, update_callback,
                          remove_callback, status_callback)
+
+    async def _store_feed_entries(self, status, feed_entries):
+        """Keep a copy of all feed entries for future lookups."""
+        if feed_entries:
+            external_ids = {entry.external_id: entry
+                            for entry in feed_entries}
+            # Override existing entries.
+            self.feed_entries.update(external_ids)
+
+    async def _update_feed_remove_entries(self, feed_external_ids):
+        """Do not remove entities."""
+        _LOGGER.debug("Not removing entries %s", feed_external_ids)
+        return 0
