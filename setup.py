228db9b6c58359d121d3fd664fc4856e44eb2acc
Malte Franken <exxamalte@users.noreply.github.com>
2019-09-24 18:15:11 +1000

    updated upstream library

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ DESCRIPTION = "An async GeoJSON client library for GeoNet NZ Volcanic Alert Leve
 URL = "https://github.com/exxamalte/python-aio-geojson-geonetnz-volcano"
 
 REQUIRES = [
-    'aio_geojson_client>=0.10b1',
+    'aio_geojson_client>=0.10b2',
     'aiohttp>=3.5.4',
     'pytz>=2019.01',
 ]
