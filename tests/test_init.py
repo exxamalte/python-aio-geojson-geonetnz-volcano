9fbbee4d40abca0a4dcf3b53db187dfa31d0a1b2
Malte Franken <exxamalte@users.noreply.github.com>
2024-10-07 01:17:43 +1100

    migrate to pyproject.toml

--- tests/test_init.py
+++
@@ -1,8 +0,0 @@
-"""Test for the GeoNet NZ Volcano GeoJSON general setup."""
-
-from aio_geojson_geonetnz_volcano import __version__
-
-
-def test_version():
-    """Test for version tag."""
-    assert __version__ is not None
